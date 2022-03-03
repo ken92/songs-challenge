636bb357c9eca4de64e5bb2e2b2cbb7cfb4ead93
BuildTools <unconfigured@null.spigotmc.org>
2022-03-03 09:17:32 -0500

    Scrolling now changes the color of the genre bar buttons. Known issue where random button will be lit up on load

--- components/genres.js
+++ components/genres.js
@@ -1,4 +1,5 @@
 import { useState, createRef, forwardRef } from 'react';
+import VisibilitySensor from 'react-visibility-sensor';
 import Genre from './genre';
 
 const GENRES = ['rap', 'pop', 'edm', 'r&b', 'rock', 'latin'];
@@ -14,18 +15,29 @@ const GenreNavButton = ({ isSelected, genre, onClick }) =>
   </button>;
 
 export default function Genres() {
-  const [selectedGenre, setSelectedGenre] = useState('');
+  const [selectedGenre, setSelectedGenre] = useState(GENRES[0]);
   const genreRefs = {};
 
   const createGenreSection = (genre) => {
     genreRefs[genre] = createRef();
-    return <Genre ref={genreRefs[genre]} key={genre} genre={genre} />;
+    return (
+      <VisibilitySensor
+        key={genre}
+        onChange={(isVisible) => {
+          if (isVisible) setSelectedGenre(genre);
+          console.log(`${genre} isVisible: ${isVisible}`);
+        }}
+      >
+        <Genre ref={genreRefs[genre]} genre={genre} />
+      </VisibilitySensor>
+    );
   };
+
   const scrollToGenre = (genre) => genreRefs[genre].current.scrollIntoView();
 
   return (
     <>
-      <div className="sticky-top p-2 bg-light">
+      <div className="sticky-top pb-2 px-2 bg-light">
         {GENRES.map(genre => (
           <GenreNavButton key={genre} genre={genre} isSelected={genre === selectedGenre} onClick={() => { scrollToGenre(genre); setSelectedGenre(genre); }} />
         ))}
